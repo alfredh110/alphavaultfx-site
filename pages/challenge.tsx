6d3f91cd64837e9e64d2d53dd6c3edef79dda4d0
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-27 13:56:47 -0400

    Delete pages directory

--- pages/challenge.tsx
+++
@@ -1,3 +0,0 @@
-export default function Challenge() {
-  return <div className="min-h-screen bg-white text-black p-10">Challenge Page</div>;
-}
--- pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div className="min-h-screen bg-black text-white p-10">Welcome to AlphaVaultFX</div>;
-}
