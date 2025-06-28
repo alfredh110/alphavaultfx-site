ffaac1da10b531bb87a86aa07ddef47b2ab2cf2a
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-28 17:36:38 -0400

    Create challenge.js

---
+++ pages/challenge.js
@@ -0,0 +1,16 @@
+import Navbar from '../components/Navbar';
+
+export default function Challenge() {
+  return (
+    <>
+      <Navbar />
+      <main style={{ padding: '2rem' }}>
+        <h2>Trading Challenge</h2>
+        <p>
+          Join our evaluation challenge, prove your consistency, and get funded!
+        </p>
+        {/* Placeholder for challenge details and enrollment */}
+      </main>
+    </>
+  );
+}
