bbafefd8b2860c4c19b76c13b732fcafd5361a00
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-28 13:46:38 -0400

    Update and rename contact to contact.js

---
+++ pages/contact.js
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold">Contact</h1>
+      <p className="mt-2">Get in touch with us at AlphaVaultFX.</p>
+    </div>
+  );
+}
