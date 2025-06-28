4a43abc8f6e6e10411e2f8cbdfabac6e99b828ab
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-28 17:44:29 -0400

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,29 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: '1rem', borderBottom: '1px solid #eee', marginBottom: '2rem' }}>
-      <Link href="/">Home</Link> |{' '}
-      <Link href="/dashboard">Dashboard</Link> |{' '}
-      <Link href="/challenge">Challenge</Link> |{' '}
-      <Link href="/auth/login">Login</Link>
+    <nav>
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <Link href="/">
+          <Image src="/logo.png" alt="AlphaVaultFX Logo" width={40} height={40} priority />
+        </Link>
+        <span style={{
+          fontWeight: 700,
+          fontSize: '1.4rem',
+          letterSpacing: '0.02em',
+          color: '#fff',
+          marginLeft: '0.5rem'
+        }}>
+          AlphaVault<span style={{ color: '#2376d1' }}>FX</span>
+        </span>
+      </div>
+      <div>
+        <Link href="/">Home</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/challenge">Challenge</Link>
+        <Link href="/auth/login">Login</Link>
+      </div>
     </nav>
   );
 }
