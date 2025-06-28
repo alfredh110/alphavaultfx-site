b69d4cf73e0dd2abf4783419586e7832bda2bf0e
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-28 13:58:30 -0400

    Update index.js

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,8 +1,8 @@
-export default function Contact() {
+export default function About() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold">Contact</h1>
-      <p className="mt-2">Get in touch with us at AlphaVaultFX.</p>
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-3xl font-bold">About Page</h1>
+      <p className="mt-2">Learn more about AlphaVaultFX.</p>
     </div>
   );
 }
