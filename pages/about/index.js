07f5edc6ee17deb30786670bafc18851621371ae
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-28 17:16:33 -0400

    Delete pages/about directory

--- pages/about/index.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-3xl font-bold">About Page</h1>
-      <p className="mt-2">Learn more about AlphaVaultFX.</p>
-    </div>
-  );
-}
