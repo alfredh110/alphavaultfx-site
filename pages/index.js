f56d5e1e4e3fec0e39f371b0f9aead05f1c63c9d
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-30 13:41:06 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Stats from '../components/Stats';
 import Features from '../components/Features';
@@ -9,15 +7,6 @@ import GetFunded from '../components/GetFunded';
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      {/* Navigation Bar */}
-      <nav style={{ marginBottom: 20 }}>
-        <ul style={{ display: 'flex', gap: 20, listStyle: 'none', padding: 0 }}>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/dashboard">Dashboard</Link></li>
-          <li><Link href="/challenge">Challenge</Link></li>
-        </ul>
-      </nav>
       <Hero />
       <Stats />
       <Features />
