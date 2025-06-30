0f8bfbad3b3e1972790339b6b2b7bc2dd696ec75
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-30 13:55:36 -0400

    Update register.js

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,19 +1,14 @@
-import Navbar from '../../components/Navbar';
-
 export default function Register() {
   return (
-    <>
-      <Navbar />
-      <main style={{ padding: '2rem' }}>
-        <h2>Register</h2>
-        <form>
-          <input type="text" placeholder="Name" required /><br />
-          <input type="email" placeholder="Email" required /><br />
-          <input type="password" placeholder="Password" required /><br />
-          <button type="submit">Register</button>
-        </form>
-        <p>Already have an account? <a href="/auth/login">Login</a></p>
-      </main>
-    </>
+    <main style={{ padding: '2rem' }}>
+      <h2>Register</h2>
+      <form>
+        <input type="text" placeholder="Name" required /><br />
+        <input type="email" placeholder="Email" required /><br />
+        <input type="password" placeholder="Password" required /><br />
+        <button type="submit">Register</button>
+      </form>
+      <p>Already have an account? <a href="/auth/login">Login</a></p>
+    </main>
   );
 }
