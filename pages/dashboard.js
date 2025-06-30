dbc7d2005fe2e871bbe6437bb39785dea5d550cd
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-30 12:49:03 -0400

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,24 @@
-import DashboardLayout from '../components/dashboard/DashboardLayout';
+import AccountOverview from '../components/AccountOverview';
+import ChallengeRules from '../components/ChallengeRules';
+import OpenTrades from '../components/OpenTrades';
+import Leaderboard from '../components/Leaderboard';
+import Payouts from '../components/Payouts';
+import NewsPanel from '../components/NewsPanel';
+import TradeAnalytics from '../components/TradeAnalytics';
+
+// ...import other existing widgets like EquityCurve, ChallengeProgress, RecentTrades, etc.
 
 export default function Dashboard() {
-  return <DashboardLayout />;
+  return (
+    <div className="dashboard-grid">
+      <AccountOverview />
+      <ChallengeRules />
+      <OpenTrades />
+      <Leaderboard />
+      <Payouts />
+      <NewsPanel />
+      <TradeAnalytics />
+      {/* Place other widgets/components here as you wish */}
+    </div>
+  );
 }
