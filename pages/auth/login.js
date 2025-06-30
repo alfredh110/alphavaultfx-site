3e6b955a4769b43f011cff8be6b7a8962e851c9b
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-30 13:43:02 -0400

    Update login.js

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,18 +1,13 @@
-import Navbar from '../../components/Navbar';
-
 export default function Login() {
   return (
-    <>
-      <Navbar />
-      <main style={{ padding: '2rem' }}>
-        <h2>Login</h2>
-        <form>
-          <input type="email" placeholder="Email" required /><br />
-          <input type="password" placeholder="Password" required /><br />
-          <button type="submit">Login</button>
-        </form>
-        <p>Don't have an account? <a href="/auth/register">Register</a></p>
-      </main>
-    </>
+    <main style={{ padding: '2rem' }}>
+      <h2>Login</h2>
+      <form>
+        <input type="email" placeholder="Email" required /><br />
+        <input type="password" placeholder="Password" required /><br />
+        <button type="submit">Login</button>
+      </form>
+      <p>Don't have an account? <a href="/auth/register">Register</a></p>
+    </main>
   );
 }
