bfb341d1c79bef9c631810a9a54598acc44d3768
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-30 12:58:55 -0400

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,24 +1,31 @@
-import AccountOverview from '../components/AccountOverview';
-import ChallengeRules from '../components/ChallengeRules';
-import OpenTrades from '../components/OpenTrades';
-import Leaderboard from '../components/Leaderboard';
-import Payouts from '../components/Payouts';
-import NewsPanel from '../components/NewsPanel';
-import TradeAnalytics from '../components/TradeAnalytics';
-
-// ...import other existing widgets like EquityCurve, ChallengeProgress, RecentTrades, etc.
+import "../styles/alphavaultfx-dashboard.css";
+import AccountOverview from "../components/AccountOverview";
+import RiskMetricsCard from "../components/RiskMetricsCard";
+import ChallengeProgressCard from "../components/ChallengeProgressCard";
+import EquityCurveCard from "../components/EquityCurveCard";
+import RecentTradesCard from "../components/RecentTradesCard";
+import LeaderboardCard from "../components/LeaderboardCard";
+import NewsPanelCard from "../components/NewsPanelCard";
+import TradeAnalyticsCard from "../components/TradeAnalyticsCard";
 
 export default function Dashboard() {
   return (
-    <div className="dashboard-grid">
-      <AccountOverview />
-      <ChallengeRules />
-      <OpenTrades />
-      <Leaderboard />
-      <Payouts />
-      <NewsPanel />
-      <TradeAnalytics />
-      {/* Place other widgets/components here as you wish */}
+    <div className="av-dashboard-wrap">
+      <h1 className="av-dashboard-title">Welcome to AlphavaultFX Dashboard</h1>
+      <div className="av-dashboard-grid">
+        <div className="av-dashboard-column">
+          <AccountOverview />
+          <RiskMetricsCard />
+          <ChallengeProgressCard />
+          <EquityCurveCard />
+        </div>
+        <div className="av-dashboard-column">
+          <RecentTradesCard />
+          <LeaderboardCard />
+          <NewsPanelCard />
+          <TradeAnalyticsCard />
+        </div>
+      </div>
     </div>
   );
 }
