d65345ddf6a6933c57840636b02db19cd2a31d41
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-30 12:10:11 -0400

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,26 +1,5 @@
-import AccountOverview from '../components/dashboard/AccountOverview';
-import EquityChart from '../components/dashboard/EquityChart';
-import RecentTrades from '../components/dashboard/RecentTrades';
-import ChallengeStatus from '../components/dashboard/ChallengeStatus';
-import PayoutsTimeline from '../components/dashboard/PayoutsTimeline';
-import RiskMetrics from '../components/dashboard/RiskMetrics';
-import Notifications from '../components/dashboard/Notifications';
+import DashboardLayout from '../components/dashboard/DashboardLayout';
 
 export default function Dashboard() {
-  return (
-    <div className="dashboard-root">
-      <Notifications />
-      <div className="dashboard-top">
-        <AccountOverview />
-        <RiskMetrics />
-      </div>
-      <EquityChart />
-      <div className="dashboard-bottom">
-        <RecentTrades />
-        <ChallengeStatus />
-        <PayoutsTimeline />
-      </div>
-      {/* Add responsive grid/flex styles */}
-    </div>
-  );
+  return <DashboardLayout />;
 }
