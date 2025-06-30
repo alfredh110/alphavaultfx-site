0efb6e7a87858e4ba998b801cc164100274ea8ea
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-30 11:47:07 -0400

    Create Hero.js

---
+++ components/Hero.js
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section style={{padding: "60px 0", textAlign: "center", color: "#fff"}}>
+      <h1 style={{fontSize: "2.5rem", fontWeight: 800}}>Welcome to AlphaVaultFX</h1>
+      <p>Unlock your trading potential.</p>
+    </section>
+  );
+}
