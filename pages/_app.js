dfb9996e16ad33da9309df2c8abb0c1b75671faf
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-30 13:18:08 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../styles/globals.css';
+import "../styles/alphavaultfx-dashboard.css";
+import React from "react";
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
