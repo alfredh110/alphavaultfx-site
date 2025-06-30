a243932378b87d2fb708f88ff2b9bbe9c6c0bfbb
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-06-30 12:47:06 -0400

    Create ChallengeRules.js

---
+++ components/ChallengeRules.js
@@ -0,0 +1,13 @@
+export default function ChallengeRules() {
+  return (
+    <div className="dashboard-card">
+      <h3>Challenge Rules</h3>
+      <ul>
+        <li>Min Trading Days: 10</li>
+        <li>Max Daily Loss: 2%</li>
+        <li>Max Loss: 8%</li>
+        <li>Profit Target: $5,000</li>
+      </ul>
+    </div>
+  );
+}
