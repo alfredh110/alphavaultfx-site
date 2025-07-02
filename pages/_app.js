0d75b7de36abfeee676d1f071d343335af0faaf1
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-07-02 13:14:24 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Or, if you use PNG: */}
+        {/* <link rel="icon" type="image/png" href="/favicon.png" /> */}
+      </Head>
       <Component {...pageProps} />
-    </Layout>
+    </>
   )
 }
