0545899424f551b9a47a61fcaaa26f1059d36287
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-07-02 12:23:56 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Hero from '../components/Hero';
-import Stats from '../components/Stats';
 import Features from '../components/Features';
 import HowToGetFunded from '../components/HowToGetFunded';
 import GetFunded from '../components/GetFunded';
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      <Stats />
       <Features />
       <HowToGetFunded />
       <GetFunded />
