99c4aa6ee63cb76d430e973341aa3d3edf460066
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-07-02 13:08:29 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import '../styles/globals.css'
-import Head from 'next/head'
+import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   )
 }
