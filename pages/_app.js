8ce7227933d39a79388c253c55296bbdefb0fbe6
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-07-02 09:49:35 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
+import { SessionProvider } from "next-auth/react";
 import '../styles/alphavaultfx-dashboard.css';
 import Navbar from '../components/Navbar';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
