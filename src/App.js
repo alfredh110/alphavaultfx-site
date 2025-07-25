4d84e4fedadb24554bc8ac6119857f90d23f37e4
alfredh110 <HILLERCAPITALINC@GMAIL.COM>
2025-07-25 15:58:09 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 import TradeCsvUpload from "./components/TradeCsvUpload";
 import TradeTable from "./components/TradeTable";
+import {
+  Container,
+  Typography,
+  Paper,
+  Box,
+  CssBaseline,
+} from "@mui/material";
+import { ProfitLossChart } from "./components/ProfitLossChart";
 
 function App() {
   const [trades, setTrades] = useState([]);
 
   return (
-    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
-      <h1>AlphaVaultFX Trading Journal</h1>
-      <TradeCsvUpload onTradesParsed={setTrades} />
-      <TradeTable trades={trades} />
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="md" sx={{ my: 4 }}>
+        <Paper elevation={3} sx={{ p: 4 }}>
+          <Typography variant="h3" component="h1" gutterBottom align="center">
+            AlphaVaultFX Trading Journal
+          </Typography>
+          <TradeCsvUpload onTradesParsed={setTrades} />
+          <Box my={4}>
+            <ProfitLossChart trades={trades} />
+          </Box>
+          <TradeTable trades={trades} />
+        </Paper>
+      </Container>
+    </React.Fragment>
   );
 }
 
